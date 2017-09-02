5e168d822f57f4bf6ed84c7aa77f766ee3fa6d6b
Bernhard Grünewaldt <github@gruenewaldt.net>
2017-09-02 14:21:20 +0200

    Update docker-entrypoint-hook.sh

--- docker-entrypoint-hook.sh
+++ docker-entrypoint-hook.sh
@@ -4,9 +4,4 @@ set -e
 
 echo "DOCKER-ENTRYPOINT-HOOK >> init"
 
-if [ "$MAVEN_REPOSITORY_MIRROR" != "false" ]
-then
-    echo "DOCKER-ENTRYPOINT-HOOK >> INJECTING MAVEN REPOSITORY MIRROR WITH URL: ${MAVEN_REPOSITORY_MIRROR}"
-    ATLAS_MAVEN_HOME=$(atlas-version | grep "ATLAS Maven Home" | awk '{print $4}') && \
-    sed -i "s@<mirrors>@<mirrors><mirror><mirrorOf>*</mirrorOf><name>remote-repos</name><url>${MAVEN_REPOSITORY_MIRROR}</url><id>remote-repos</id></mirror>@g" $ATLAS_MAVEN_HOME/conf/settings.xml
-fi
+source ~/.sdkman/bin/sdkman-init.sh
